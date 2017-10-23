9fa2c7f177536534aa0b9c56593a24287295d393
Alan Saber <Alan Saber>
2017-10-24 00:15:19 +0200

    Delete/Update

--- src/App.js
+++ src/App.js
@@ -11,23 +11,25 @@ class App extends Component {
     super(props);
     this.state = {
       recipes: this.props.recipes, 
-      modal: false,
-      showModal: false,
+      showAddModal: false,
+      showEditModal: false,
       newRecipeName: '',
-      newRecipeIngredients: '',
+      newRecipeIngredients: [],
       currentRecipeName: '',
-      currentRecipePlan: ''
+      currentRecipePlan: '',
+      recipeId: ''
     };
     this.toggleRecipeModal = this.toggleRecipeModal.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.addRecipe = this.addRecipe.bind(this);
     this.toggleEditRecipeModal = this.toggleEditRecipeModal.bind(this);
     this.deleteReceipe = this.deleteReceipe.bind(this);
+    this.editRecipe = this.editRecipe.bind(this);
   }
 
   toggleRecipeModal() {
     this.setState({
-      modal: !this.state.modal
+      showAddModal: !this.state.showAddModal
     });
   }
 
@@ -42,9 +44,15 @@ class App extends Component {
 
   toggleEditRecipeModal(recipe){
    this.setState({
-      showModal: !this.state.showModal,
+      showEditModal: !this.state.showEditModal,
    });
-    
+   if(recipe !== undefined){
+    this.setState({
+      newRecipeName: recipe.name,
+      newRecipeIngredients: recipe.plan,
+      recipeId: this.state.recipes.indexOf(recipe)
+    });
+   } 
   }
 
   addRecipe(event) {
@@ -63,8 +71,31 @@ class App extends Component {
     this.toggleRecipeModal();
   }
 
-  deleteReceipe() {
-    console.log("Deleted");
+  editRecipe(event) {
+    event.preventDefault();
+    const newState = this.state.recipes;
+    let recipe = this.state.newRecipeName;
+    let ingredients = this.state.newRecipeIngredients;
+    if(!Array.isArray(ingredients)) {
+     ingredients = ingredients.split(',');
+    }
+    let editedRecipe = {name: recipe, plan: ingredients};
+    newState.splice(this.state.recipeId, 1, editedRecipe);
+    this.setState({
+      recipe: newState
+    });
+    this.toggleEditRecipeModal();
+  }
+
+  deleteReceipe(recipe) {
+    const newState = this.state.recipes;
+    if(this.state.recipes.indexOf(recipe) !== -1){
+      newState.splice(newState.indexOf(recipe), 1);
+      this.setState({
+      recipe: newState
+    });
+    }
+    
   }
 
   render() {
@@ -77,7 +108,7 @@ class App extends Component {
             <ListGroupItem key={item.name}>
               <div className="clearfix">
                 <h5 className="float-left">{item.name}</h5>
-                <small className="float-right">{item.plan.map(ingredients =>  ingredients + ' ')}</small>
+                <small className="float-right">{item.plan.map(ingredients =>  `-${ingredients} `)}</small>
               </div>
               <hr/>
               <div className="clearfix">
@@ -94,7 +125,7 @@ class App extends Component {
         </div>
         <br/>
         <Footer></Footer>
-        <Modal isOpen={this.state.modal} toggle={this.toggleRecipeModal} className={this.props.className}>
+        <Modal isOpen={this.state.showAddModal} toggle={this.toggleRecipeModal} className={this.props.className}>
           <ModalHeader toggle={this.toggleRecipeModal}>Add Recipe</ModalHeader>
           <Form onSubmit={this.addRecipe}>
           <ModalBody>
@@ -112,16 +143,22 @@ class App extends Component {
           </Form>
         </Modal>
 
-        <Modal isOpen={this.state.showModal} toggle={this.toggleEditRecipeModal} className={this.props.className}>
+        <Modal isOpen={this.state.showEditModal} toggle={this.toggleEditRecipeModal} className={this.props.className}>
           <ModalHeader toggle={this.toggleEditRecipeModal}>Recipe</ModalHeader>
+          <Form onSubmit={this.editRecipe}>
           <ModalBody>
-               
+            <Label for="recipeName">Name:</Label>
+            <Input name="recipeName" value={this.state.newRecipeName} onChange={this.handleChange} id="recipename"/>
+            <Label for="ingredients">Ingredients:</Label>
+            <Input name="ingredients" value={this.state.newRecipeIngredients} onChange={this.handleChange} type="textarea"/>
           </ModalBody>
           <ModalFooter>
             <div className="mx-auto">
+              <Button color="primary" type="submit">Save</Button>{' '}
               <Button color="danger" onClick={this.toggleEditRecipeModal}>Close</Button>
             </div>
           </ModalFooter>
+          </Form>
         </Modal>
         
       </div>
